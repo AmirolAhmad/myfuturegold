9fc82dc57b7c2e1eb3563fac6c899e86b88a6fe6
AmirolAhmad <9w2pdf@gmail.com>
2014-09-09 17:05:30 +0800

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ lock '3.2.1'
 
 set :repo_url, 'git@github.com:amirolahmad/myfuturegold.git'
 
+set :deploy_to, '/home/deploy/myfuturegold'
+
 # set :rbenv_custom_path, '/opt/rbenv'
 set :rbenv_type, :user
 set :rbenv_ruby, '2.1.2'
