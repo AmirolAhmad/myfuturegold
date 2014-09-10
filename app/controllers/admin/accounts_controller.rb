c53b109475f9da9f6d5122f7a7307e982381c9ea
AmirolAhmad <9w2pdf@gmail.com>
2014-09-10 14:45:51 +0800

    list all user at index page

--- app/controllers/admin/accounts_controller.rb
+++ app/controllers/admin/accounts_controller.rb
@@ -2,13 +2,14 @@ class Admin::AccountsController < ApplicationController
   before_filter :store_location, only: [:index]
   before_filter :require_admin
 
+  def index
+    @users = User.all.order("created_at DESC")
+  end
+
   def new
   end
 
   def create
-  end
-
-  def index
   end
 
   def edit
