6d3105d3b270a68e325dca96b8714ac0cf260949
AmirolAhmad <9w2pdf@gmail.com>
2014-09-16 22:45:55 +0800

    add paranoid to profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,6 +1,8 @@
 class Profile < ActiveRecord::Base
 	belongs_to :user, foreign_key: :user_id
 
+	acts_as_paranoid
+
 	validates_presence_of :user_id
 	# validates_presence_of :nama_penuh
 	# validates_presence_of :tel_num
