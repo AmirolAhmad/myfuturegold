4031c57237eaeff675608277e39c41fd2ab6525a
AmirolAhmad <9w2pdf@gmail.com>
2014-09-23 02:47:26 +0800

    added json api to the order controller

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,26 @@
 class Admin::OrdersController < ApplicationController
+  before_filter :set_user, only: [:show]
 	before_filter :store_location, only: [:index]
   before_filter :require_admin
   
   def index
     @orders = Order.all.order("created_at DESC")
+    respond_to do |format|
+      format.html { @orders }
+      format.json { render json: @orders.to_json(include: [:status, :package, :discount]) }
+    end
   end
 
   def show
-    @order = Order.find(params[:id])
+    respond_to do |format|
+      format.html { @order }
+      format.json { render json: @order.to_json(include: [:status, :package, :discount]) }
+    end
   end
 
   private
+
+  def set_user
+    @order = Order.find(params[:id])
+  end
 end
