b99efafbf8fcde1cfc349c4277a1a9ada82f2924
AmirolAhmad <9w2pdf@gmail.com>
2014-09-26 00:10:38 +0800

    [BUG] fixed create order via client account

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -31,7 +31,7 @@ class Admin::OrdersController < ApplicationController
   def create
     @order = Order.new(order_params, user: @user)
     if @order.save
-      redirect_to admin_user_orders_path, notice: "Ref Number has been created."
+      redirect_to admin_user_orders_path, notice: "New order has been created."
 
       # total price
       @total_price = @order.price.to_i * @order.gram_quantity.to_i
@@ -41,6 +41,10 @@ class Admin::OrdersController < ApplicationController
       random = [('A'..'Z'), ('1'..'9')].map { |i| i.to_a }.flatten
       ref_number = (0...10).map { random[rand(random.length)] }.join
       @order.update_attributes(:ref_number => ref_number)
+
+      #ordered date
+      @ordered_date = Time.now
+      @order.update_attributes(:ordered_date => @ordered_date)
     else
       render 'new'
     end
