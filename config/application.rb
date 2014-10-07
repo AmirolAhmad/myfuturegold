0229f23363dcb0edce9835f022050e616074b4e7
AmirolAhmad <9w2pdf@gmail.com>
2014-10-08 04:38:23 +0800

    change time zone format

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Myfuturegold
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = 'Kuala Lumpur'
+    config.time_zone = 'Asia/Kuala_Lumpur'
     config.encoding = "utf-8"
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
