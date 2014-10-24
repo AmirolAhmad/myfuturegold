aae59614067b2c4deda391b4563ea681c495c971
AmirolAhmad <9w2pdf@gmail.com>
2014-10-24 23:34:39 +0800

    add default value to admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008083708) do
+ActiveRecord::Schema.define(version: 20141024153319) do
 
   create_table "discounts", force: true do |t|
     t.string   "discount_date"
@@ -105,16 +105,16 @@ ActiveRecord::Schema.define(version: 20141008083708) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "login",                               null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "login",                                  null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin"
+    t.boolean  "admin",                  default: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
