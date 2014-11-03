27f0347be4ed88b4e29e9e8a312c258efed848d5
AmirolAhmad <9w2pdf@gmail.com>
2014-11-04 00:21:33 +0800

    add validation to payment & voucher

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,6 +2,8 @@ class Payment < ActiveRecord::Base
   belongs_to :user, foreign_key: :user_id
   belongs_to :order, foreign_key: :order_id
 
+  validates :total_payment, numericality: { only_integer: true }
+
   def self.total_on(date)
     where("date(created_at) = ?", date).sum(:total_payment)
   end
