ece096dd72576685cd5b07d386b47f1333fe614b
AmirolAhmad <9w2pdf@gmail.com>
2014-11-04 00:14:26 +0800

    add validation

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -11,9 +11,18 @@ class Profile < ActiveRecord::Base
 	# validates_presence_of :tel_num_waris
 	# validates_presence_of :nama_bank
 	# validates_presence_of :nama_akaun
+	validates 			:facebook_id, format: { with: /\A[-\w.]*\z/ }, allow_blank: true
+	validates 			:heir1_fb_id, format: { with: /\A[-\w.]*\z/ }, allow_blank: true
+	validates 			:heir2_fb_id, format: { with: /\A[-\w.]*\z/ }, allow_blank: true
 	validates_numericality_of :no_akaun, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
+	validates_numericality_of :postcode, length: { is: 5 }, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
+	validates_numericality_of :heir1_postcode, length: { is: 5 }, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
+	validates_numericality_of :heir2_postcode, length: { is: 5 }, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
 	validates 			:ic_number, length: { is: 12 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
+	validates 			:ic_num_heir1, length: { is: 12 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
+	validates 			:ic_num_heir2, length: { is: 12 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
 	validates           :tel_num, length: { in: 8..15 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
 	validates           :tel_num_waris, length: { in: 8..15 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
+	validates           :heir2_phone_num, length: { in: 8..15 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
 	
 end
