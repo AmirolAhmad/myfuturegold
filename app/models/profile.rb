ab425bc1ce47dd9fe4f43cfb8deb5a27b8f12640
AmirolAhmad <9w2pdf@gmail.com>
2014-11-06 23:37:14 +0800

    make ic number and phone number as numeracality

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -18,11 +18,11 @@ class Profile < ActiveRecord::Base
 	validates_numericality_of :postcode, length: { is: 5 }, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
 	validates_numericality_of :heir1_postcode, length: { is: 5 }, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
 	validates_numericality_of :heir2_postcode, length: { is: 5 }, allow_blank: true, :only_integer => true, :message => 'should only contain numbers.'
-	validates 			:ic_number, length: { is: 12 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
-	validates 			:ic_num_heir1, length: { is: 12 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
-	validates 			:ic_num_heir2, length: { is: 12 }, format: { with: /\A[0-9\-\+ \/]*\z/ }, allow_blank: true
-	validates           :tel_num, length: { in: 8..15 }, format: { with: /\A0[0-9\-\+ \/]*\z/ }, allow_blank: true
-	validates           :tel_num_waris, length: { in: 8..15 }, format: { with: /\A0[0-9\-\+ \/]*\z/ }, allow_blank: true
-	validates           :heir2_phone_num, length: { in: 8..15 }, format: { with: /\A0[0-9\-\+ \/]*\z/ }, allow_blank: true
+	validates 			:ic_number, length: { is: 12 }, :numericality => true, allow_blank: true
+	validates 			:ic_num_heir1, length: { is: 12 }, :numericality => true, allow_blank: true
+	validates 			:ic_num_heir2, length: { is: 12 }, :numericality => true, allow_blank: true
+	validates           :tel_num, length: { in: 8..15 }, format: { with: /\A0[0-9\-\+ \/]*\z/ }, allow_blank: true, :numericality => true
+	validates           :tel_num_waris, length: { in: 8..15 }, format: { with: /\A0[0-9\-\+ \/]*\z/ }, allow_blank: true, :numericality => true
+	validates           :heir2_phone_num, length: { in: 8..15 }, format: { with: /\A0[0-9\-\+ \/]*\z/ }, allow_blank: true, :numericality => true
 	
 end
