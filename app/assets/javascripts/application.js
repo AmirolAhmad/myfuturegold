3b9bf5b6e79c624aa20afc3b56273901dcb84598
AmirolAhmad <9w2pdf@gmail.com>
2014-11-07 23:53:21 +0800

    make the nabber off canvas in mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require jasny-bootstrap
 //= require html5shiv
 //= require respond.min
 //= require jquery.dataTables
@@ -28,6 +29,10 @@
 
 $(document).ready( function () {
     $(".btn").tooltip({
+    	placement : 'bottom',
+    	container: 'body'
+    });
+    $(".inboxPop").tooltip({
     	placement : 'bottom',
     	container: 'body'
     });
