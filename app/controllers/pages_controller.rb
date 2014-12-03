2222f85719fd7a803598552a8c281f5b30aa93ce
AmirolAhmad <9w2pdf@gmail.com>
2014-12-03 22:10:50 +0800

    #39 add grand total to client dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,9 @@ class PagesController < ApplicationController
     @announcement = Announcement.first
     @order = Order.where(user_id: @user).to_a
 
+    @tw = @user.orders.where('package_id' => 6).sum(:gram_quantity).to_i
+    @td = @user.orders.where('package_id' => 6).sum(:total_discount).to_i
+
     @disable_header = true
   end
 
