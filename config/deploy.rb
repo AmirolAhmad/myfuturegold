066ff39837713339992bbcec004d6dea0c92d599
AmirolAhmad <9w2pdf@gmail.com>
2014-12-04 21:54:59 +0800

    add require capistrano gitflow

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,9 @@
 # config valid only for Capistrano 3.1
 #lock '3.2.1'
 
+# require 'capistrano/ext/multistage'
+require 'capistrano/gitflow'
+
 set :application, 'myfuturegold'
 set :repo_url, 'git@github.com:AmirolAhmad/myfuturegold.git'
 
