5b56cfbc785efc9f5639b41e46d0512b7225d321
AmirolAhmad <9w2pdf@gmail.com>
2014-12-07 21:39:30 +0800

    grand total only for active order

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,12 +8,22 @@ class PagesController < ApplicationController
     @order = Order.where(user_id: @user).to_a
 
     # orders = Order.all
-    @tw = Order.where('package_id' => 6, user_id: @user).sum(:gram_quantity).to_i
-    @td = Order.where('package_id' => 6, user_id: @user).sum(:total_discount).to_i
+    @tw = grand_total_weight
+    @td = grand_total_discount
 
     @disable_header = true
   end
 
   def about
+  end
+
+  private  
+
+  def grand_total_weight
+    Order.where('package_id' => 6, 'status_id' => 1, user_id: current_user).sum(:gram_quantity).to_i
+  end
+
+  def grand_total_discount
+    Order.where('package_id' => 6, 'status_id' => 1, user_id: @user).sum(:total_discount).to_i
   end
 end
