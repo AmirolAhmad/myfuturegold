6ca48eafa48fed53680886bd010341604a6b2130
AmirolAhmad <9w2pdf@gmail.com>
2014-12-08 12:09:38 +0800

    order helper added

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,19 @@
 module OrdersHelper
+
+  def order_active(order)
+		order.status.status_name == "Active"
+	end
+
+  def order_inactive(order)
+		order.status.status_name == "Inactive"
+	end
+
+  def order_pending(order)
+		order.status.status_name == "Pending"
+	end
+
+  def order_buyback(order)
+		order.status.status_name == "Buyback"
+	end
+
 end
